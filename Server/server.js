1dfa8fab44eafcc603e47dbe168acb6ee0f643d5
Shubham Singh <singhofficial1611@gmail.com>
2025-05-20 17:41:33 +0530

    connect DB

--- Server/server.js
+++ Server/server.js
@@ -1,13 +1,18 @@
-import express from 'express';
+import express from "express";
+import dotenv from "dotenv";
+import connectDB from "./DB/DB.js" // Your MongoDB connection
+// import authRoutes from "./routes/authRoutes.js";
+
+dotenv.config();
+connectDB();
 
 const app = express();
+app.use(express.json()); // Allow JSON payloads
 
-app.use(express.json());
+// app.use("/api/auth", authRoutes);
 
-app.get('/', (req, res) => {
-    res.send('Working!');
+
+app.listen(process.env.PORT,()=>{
+    console.log(`Server is running on port ${process.env.PORT}`);
+    connectDB();
 });
-app.listen(3000, () => {
-    console.log('Server is running on port 3000');
-}
-);
