52965393783e0ed6edc4b232fbb57a18c8b604f4
Shubham Singh <singhofficial1611@gmail.com>
2025-10-01 10:18:07 +0530

    completed project

--- Server/models/chatRoomSchema.js
+++
@@ -1,9 +0,0 @@
-const chatRoomSchema = new mongoose.Schema({
-  roomName: { type: String, required: true }, // For named group chats
-  participants: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }], // List of users in the chat
-  createdAt: { type: Date, default: Date.now },
-  lastMessage: { type: mongoose.Schema.Types.ObjectId, ref: "Message" }, // Reference to last sent message
-});
-
-const ChatRoom = mongoose.model("ChatRoom", chatRoomSchema);
-export default ChatRoom;
